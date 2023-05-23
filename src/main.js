0cddb52b2de167e56b1634cdc4d5086b08c9965f
sergioATG <124618501+sergioATG@users.noreply.github.com>
2023-05-23 10:29:55 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+// Importar las dependencias necesarias
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.112.1/build/three.module.js';
 import 'https://cdn.jsdelivr.net/npm/simplex-noise@2.4.0/simplex-noise.js';
+
+// Importar los módulos personalizados
 import {clouds} from './clouds.js';
 import {controls} from './controls.js';
 import {game} from './game.js';
@@ -8,22 +11,25 @@ import {math} from './math.js';
 import {textures} from './textures.js';
 import {voxels} from './voxels.js';
 
-
 let _APP = null;
 
-
+// Clase principal del juego que extiende de la clase base 'game.Game'
 class SimonDevCraft extends game.Game {
   constructor() {
     super();
   }
 
+  // Método llamado al inicializar el juego
   _OnInitialize() {
     this._entities = {};
 
+    // Cargar el fondo del juego
     this._LoadBackground();
 
+    // Crear una nueva instancia del 'TextureAtlas' y esperar a que cargue
     this._atlas = new textures.TextureAtlas(this);
     this._atlas.onLoad = () => {
+      // Crear las entidades del juego una vez que el atlas de texturas haya cargado
       this._entities['_voxels'] = new voxels.SparseVoxelCellManager(this);
       this._entities['_clouds'] = new clouds.CloudManager(this);
       this._entities['_controls'] = new controls.FPSControls(
@@ -35,6 +41,7 @@ class SimonDevCraft extends game.Game {
     };
   }
 
+  // Cargar el fondo del juego utilizando una textura cubemap
   _LoadBackground() {
     const loader = new THREE.CubeTextureLoader();
     const texture = loader.load([
